768aabc491bbdd7efa087b45569a2bae2dfd2e4a
carlhauck <carlhauck@gmail.com>
2020-05-27 12:39:31 -0500

    adds products route and corresponding view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/products" => "products#see_all_products"
+  end
 end

500db220c0ae3935da821130064b26e3eb5a897c
carlhauck <carlhauck@gmail.com>
2020-05-27 12:12:41 -0500

    adds example products to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: "fly swatter", price: 1, image_url: "https://res.cloudinary.com/forallpromos/image/fetch/f_auto/https://www.4allpromos.com/sites/default/files/imagecache/420x420/candypress/ProdImages/large/388-1034.jpg", description: "Use this to swat flies.")
+product.save
+
+product = Product.new(name: "chair", price: 100, image_url: "https://images.homedepot-static.com/productImages/3bb8e409-324c-419b-8c55-fd02ed3e8957/svn/cherry-wood-seat-cherry-wood-frame-flash-furniture-dining-chairs-xudgw0008vrtchy-64_600.jpg", description: "Sit down and relax.")
+product.save

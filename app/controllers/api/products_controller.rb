400c31fe089596b6d71c4662dd7b39f0c16b372d
carlhauck <carlhauck@gmail.com>
2020-05-27 12:46:44 -0500

    adds routes to display individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,5 +4,15 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.json.jb'
   end
+  
+  def see_fly_swatter
+    @product = Product.find_by(name: "fly swatter")
+    render 'fly_swatter.json.jb'
+  end
+  
+  def see_newest_product
+    @product = Product.last
+    render 'newest_product.json.jb'
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/products" => "products#see_all_products"
+    get "/fly_swatter" => "products#see_fly_swatter"
+    get "/newest_product" => "products#see_newest_product"
   end
 end

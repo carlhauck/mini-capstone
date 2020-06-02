963cd5445f9269b12c79367648e194093e1c8ea0
carlhauck <carlhauck@gmail.com>
2020-06-02 13:08:26 -0500

    adds products to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-product = Product.new(name: "fly swatter", price: 1, image_url: "https://res.cloudinary.com/forallpromos/image/fetch/f_auto/https://www.4allpromos.com/sites/default/files/imagecache/420x420/candypress/ProdImages/large/388-1034.jpg", description: "Use this to swat flies.")
-product.save
+# product = Product.new(name: "fly swatter", price: 1, image_url: "https://res.cloudinary.com/forallpromos/image/fetch/f_auto/https://www.4allpromos.com/sites/default/files/imagecache/420x420/candypress/ProdImages/large/388-1034.jpg", description: "Use this to swat flies.")
+# product.save
 
-product = Product.new(name: "chair", price: 100, image_url: "https://images.homedepot-static.com/productImages/3bb8e409-324c-419b-8c55-fd02ed3e8957/svn/cherry-wood-seat-cherry-wood-frame-flash-furniture-dining-chairs-xudgw0008vrtchy-64_600.jpg", description: "Sit down and relax.")
-product.save
+Product.create(name: "Sweatpants", price: 15, image_url: "https://contestimg.wish.com/api/webimage/5ba44688236cce377bb52685-large.jpg?cache_buster=978868a7abef943d5963ca125b8b4c78", description: "Stylish and comfortable. Keep the bees away from your legs")

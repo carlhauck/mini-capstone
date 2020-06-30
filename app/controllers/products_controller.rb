b0909d1c50722f4175fa3a991c8ade171a0ec321
carlhauck <carlhauck@gmail.com>
2020-06-30 15:33:11 -0500

    adds public index html/css/js files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,13 +5,13 @@ class ProductsController < ApplicationController
     render "index.html.erb"
   end
 
-  def new
-    render "new.html.erb"
-  end
-
   def show
     @product = Product.find(params[:id])
     render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
   end
 
   def create

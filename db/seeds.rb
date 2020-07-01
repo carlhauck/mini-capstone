6ce1625e0856ff0cb862cde171bdc41bbee19724
carlhauck <carlhauck@gmail.com>
2020-07-01 18:01:10 -0500

    modifies seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+Supplier.create!([
+  {name: "National Treasure Warehouse", email: "ntwarehouse@gmail.com", phone_number: "528-283-4297"},
+  {name: "Household Supply 4 U", email: "householdsupply4u@hotmail.com", phone_number: "529-777-5254"},
+  {name: "Arcades R Us", email: "warehouse@arcadesrus.com", phone_number: "713-443-7159"}
+])
+Product.create!([
+  {name: "Window", price: "300.0", description: "See through it! Keep birds out!", in_stock?: true, supplier_id: 2},
+  {name: "Table", price: "150.0", description: "Set things on it and they won't fall through and hit the floor.", in_stock?: true, supplier_id: 2},
+  {name: "Sweatpants", price: "25.0", description: "Keep the bees away from your legs.", in_stock?: true, supplier_id: 1},
+  {name: "Used Big Bertha Arcade Game", price: "950.0", description: "Make your home feel a little more like Chuck E. Cheese.", in_stock?: true, supplier_id: 3},
+  {name: "Fly swatter", price: "1.5", description: "Want to make that ceaseless buzzing stop?", in_stock?: true, supplier_id: 2},
+  {name: "Chair", price: "105.0", description: "Take a load off, friend.", in_stock?: true, supplier_id: 2}
+])
+User.create!([
+  {first_name: "Carl", last_name: "Hauck", email: "carlhauck@gmail.com", password_digest: "$2a$12$4G4yOdHn5dT5I3p359nL5Obl0y30zEYFal0tmiMnejd.pXdDruDeG", admin: false},
+  {first_name: "Abi", last_name: "Humber", email: "crabigail@gmail.com", password_digest: "$2a$12$jGNdaV8mSO8j9VpHyZgYse7QqYmRyWB7gOlDGm3rrrdMFK04lHEPq", admin: false},
+  {first_name: "Admin", last_name: "Jones", email: "adminjones@gmail.com", password_digest: "$2a$12$PyXMngODMx5UAZgJ4xtWKehcbAgGBvljgHnzmIBaEA1Q3D.wpdOl.", admin: true}
+])
 CartedProduct.create!([
   {user_id: 1, product_id: 1, quantity: 2, status: "purchased", order_id: 7},
   {user_id: 1, product_id: 1, quantity: 2, status: "purchased", order_id: 7},
@@ -29,14 +47,6 @@ Order.create!([
   {user_id: 1, subtotal: "409.0", tax: "41.92", total: "450.92"},
   {user_id: 1, subtotal: "409.0", tax: "41.92", total: "450.92"}
 ])
-Product.create!([
-  {name: "Window", price: "300.0", description: "See through it! Keep birds out!", in_stock?: true, supplier_id: 2},
-  {name: "Table", price: "150.0", description: "Set things on it and they won't fall through and hit the floor.", in_stock?: true, supplier_id: 2},
-  {name: "Sweatpants", price: "25.0", description: "Keep the bees away from your legs.", in_stock?: true, supplier_id: 1},
-  {name: "Used Big Bertha Arcade Game", price: "950.0", description: "Make your home feel a little more like Chuck E. Cheese.", in_stock?: true, supplier_id: 3},
-  {name: "Fly swatter", price: "1.5", description: "Want to make that ceaseless buzzing stop?", in_stock?: true, supplier_id: 2},
-  {name: "Chair", price: "105.0", description: "Take a load off, friend.", in_stock?: true, supplier_id: 2}
-])
 ProductCategory.create!([
   {product_id: 1, category_id: 2},
   {product_id: 2, category_id: 2},
@@ -44,14 +54,4 @@ ProductCategory.create!([
   {product_id: 13, category_id: 1},
   {product_id: 12, category_id: 2},
   {product_id: 14, category_id: 3}
-])
-Supplier.create!([
-  {name: "National Treasure Warehouse", email: "ntwarehouse@gmail.com", phone_number: "528-283-4297"},
-  {name: "Household Supply 4 U", email: "householdsupply4u@hotmail.com", phone_number: "529-777-5254"},
-  {name: "Arcades R Us", email: "warehouse@arcadesrus.com", phone_number: "713-443-7159"}
-])
-User.create!([
-  {first_name: "Carl", last_name: "Hauck", email: "carlhauck@gmail.com", password_digest: "$2a$12$4G4yOdHn5dT5I3p359nL5Obl0y30zEYFal0tmiMnejd.pXdDruDeG", admin: false},
-  {first_name: "Abi", last_name: "Humber", email: "crabigail@gmail.com", password_digest: "$2a$12$jGNdaV8mSO8j9VpHyZgYse7QqYmRyWB7gOlDGm3rrrdMFK04lHEPq", admin: false},
-  {first_name: "Admin", last_name: "Jones", email: "adminjones@gmail.com", password_digest: "$2a$12$PyXMngODMx5UAZgJ4xtWKehcbAgGBvljgHnzmIBaEA1Q3D.wpdOl.", admin: true}
 ])
